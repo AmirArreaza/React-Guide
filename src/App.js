c5730c75d0b587955c6b0024a530e9696edb3a06
AmirArreaza <ajaarreazav@gmail.com>
2020-06-07 22:00:50 +0100

    Use of Hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, I'm a React App</h1>
+const app = (props) => {
 
-        <Person name="Amir" age="20"/>
-        <Person name="Amir" age="25">Ta Loco!</Person>
-        <Person name="Amir" age="30"/>
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Amir', age: 28},
+      { name: 'Max', age: 30},
+      { name: 'Manu', age: 21}
+    ],
+    otherState: 'Otro'
+  });
 
-      </div>
-    );
-  }
+  const[otherState, setOtherState] = useState('Otro Mas');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    //console.log('Alive');
+    //Don't Use this: this.state.persons[0].name = 'Test';
+    setPersonsState({persons: [
+      { name: 'AmirDos', age: 28},
+      { name: 'Max', age: 900},
+      { name: 'Manu', age: 21}
+      ],
+      otherState: 'Otro'
+    })
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello, I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Ta Loco!</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
 }
 
-export default App;
+export default app;
