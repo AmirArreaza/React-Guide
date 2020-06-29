19fbb17c151045e4d0b93ade68c85386255ef47b
AmirArreaza <ajaarreazav@gmail.com>
2020-06-29 14:21:24 +0100

    Pending change preventing npm eject to run

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-background-color: ${props => props.alt ? 'red' : 'green'};
-color: white;
-font: inherit;
-border: 1px solid blue;
-padding: 8px;
-cursor: pointer;
-
-&:hover {
-  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-  color: black;
-}
-`;
 
 class App extends Component {
 
@@ -102,14 +88,6 @@ class App extends Component {
           }
         </div>
       );
-
-      /*
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
-      */
     }
 
     const classes = [];
@@ -124,9 +102,7 @@ class App extends Component {
         <div className="App">
           <h1>Hello, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
-          <StyledButton 
-            alt={this.state.showPersons} 
-            onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+          <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
     );
